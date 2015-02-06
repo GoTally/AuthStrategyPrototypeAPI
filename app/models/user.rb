d10b4bbadb70cbfe60a63042c2f97a424803092d
Tyler Ewing <tewing10@gmail.com>
2015-02-05 22:32:35 -0500

    Update User model with creation from SocialIdentity

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
 
   has_many :tokens
   has_many :social_identities
+
+  def self.create_from_social_identity(social_identity_params)
+    user_params = social_identity_params.slice(:first_name, :last_name, :email)
+    User.new(user_params).tap do |user|
+      s = SocialIdentity.create(social_identity_params)
+      user.social_identities << s
+      user.save!
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,37 @@
 require 'rails_helper'
 
-describe User, 'Validations' do
-  it { should validate_uniqueness_of(:email) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:first_name) }
-  it { should validate_presence_of(:last_name) }
-end
+describe User do
+  describe 'Validations' do
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
 
-describe User, 'Associations' do
-  it { should have_many(:tokens) }
-  it { should have_many(:social_identities) }
+  describe 'Associations' do
+    it { should have_many(:tokens) }
+    it { should have_many(:social_identities) }
+  end
+
+  describe '::create_from_provider' do
+    subject(:user){ User.create_from_social_identity(social_identity_params) }
+    let(:social_identity_params){
+      {
+        uid: '10153057521702250',
+        auth_token: 'good-token',
+        first_name: 'Jeff',
+        last_name: 'Fisher',
+        email: 'im4ussoccer@yahoo.com',
+        provider: 'facebook'
+      }
+    }
+
+    it 'should create a user and social identity' do
+      expect(user.first_name).to eq('Jeff')
+      expect(user.last_name).to eq('Fisher')
+      expect(user.email).to eq('im4ussoccer@yahoo.com')
+      expect(user.social_identities.count).to eq(1)
+      expect(user.social_identities.first.uid).to eq('10153057521702250')
+    end
+  end
 end
